1cc9806311e2cd906e5c01378044696b68ce4b16
kazimpashayev <kazimpasayev@protonmail.com>
2023-11-26 21:25:39 +0400

    Updated the component

--- src/context/tictactoe.jsx
+++ src/context/tictactoe.jsx
@@ -1,48 +1,23 @@
 import { createContext, useState, useEffect } from 'react';
+import GameStatus from '../components/gameStatus';
+import { winningRules } from '../components/winningRules';
 
 const TictactoeContext = createContext();
 
 const Player_X = 'X';
 const Player_O = 'O';
 
-function checkWinner(squares, setWinningLineClass) {
-  for (const winningRule of winningRules) {
-    const { rule, winningLineClass } = winningRule;
-    // console.log(squares[rule[0]]);
-    const squareValue1 = squares[rule[0]];
-    const squareValue2 = squares[rule[1]];
-    const squareValue3 = squares[rule[2]];
-
-    if (
-      squareValue1 !== null &&
-      squareValue1 === squareValue2 &&
-      squareValue1 === squareValue3
-    ) {
-      setWinningLineClass(winningLineClass);
-    }
-  }
-}
-
-const winningRules = [
-  // Rows
-  { rule: [0, 1, 2], winningLineClass: 'winning-line-row-1' },
-  { rule: [3, 4, 5], winningLineClass: 'winning-line-row-2' },
-  { rule: [6, 7, 8], winningLineClass: 'winning-line-row-3' },
-
-  // Columns
-  { rule: [0, 3, 6], winningLineClass: 'winning-line-col-1' },
-  { rule: [1, 4, 7], winningLineClass: 'winning-line-col-2' },
-  { rule: [2, 5, 8], winningLineClass: 'winning-line-col-3' },
-
-  // Diagonals
-  { rule: [0, 4, 8], winningLineClass: 'winning-line-diagonal-1' },
-  { rule: [2, 4, 6], winningLineClass: 'winning-line-diagonal-2' },
-];
+//! Commit Olunmalidir!
 
 function Provider({ children }) {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState(Player_X);
   const [winningLineClass, setWinningLineClass] = useState('');
+  const [gameStatus, setGameStatus] = useState(GameStatus.inProgress);
+
+  useEffect(() => {
+    checkWinner(squares, setWinningLineClass, setGameStatus);
+  }, [squares]);
 
   const handleSquareClick = (index) => {
     // console.log(index);
@@ -62,13 +37,31 @@ function Provider({ children }) {
     }
   };
 
-  useEffect(() => {
-    checkWinner(squares, setWinningLineClass);
-  }, [squares]);
+  const checkWinner = (squares, setWinningLineClass, setGameStatus) => {
+    winningRules.forEach(({ rule, winningLineClass }) => {
+      // console.log(squares[rule[0]]);
+      const squareValue1 = squares[rule[0]];
+      const squareValue2 = squares[rule[1]];
+      const squareValue3 = squares[rule[2]];
+
+      if (
+        squareValue1 !== null &&
+        squareValue1 === squareValue2 &&
+        squareValue1 === squareValue3
+      ) {
+        setWinningLineClass(winningLineClass);
+        if (squareValue1 === Player_X) {
+          setGameStatus(GameStatus.playerXWins);
+        } else {
+          setGameStatus(GameStatus.playerOWins);
+        }
+      }
+    });
+  };
 
   return (
     <TictactoeContext.Provider
-      value={{ squares, handleSquareClick, winningLineClass }}
+      value={{ squares, handleSquareClick, winningLineClass, gameStatus }}
     >
       {children}
     </TictactoeContext.Provider>
