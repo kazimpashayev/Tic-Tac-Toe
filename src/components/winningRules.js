945d0571e2e6316e9d22db3737d01f5f8db7a338
kazimpashayev <kazimpasayev@protonmail.com>
2023-11-27 16:04:08 +0400

    Refactored all components

---
+++ src/components/winningRules.js
@@ -0,0 +1,15 @@
+export const winningRules = [
+  // Rows
+  { rule: [0, 1, 2], winningLineClass: 'winning-line-row-1' },
+  { rule: [3, 4, 5], winningLineClass: 'winning-line-row-2' },
+  { rule: [6, 7, 8], winningLineClass: 'winning-line-row-4' },
+
+  // Columns
+  { rule: [0, 3, 6], winningLineClass: 'winning-line-col-1' },
+  { rule: [1, 4, 7], winningLineClass: 'winning-line-col-2' },
+  { rule: [6, 7, 8], winningLineClass: 'winning-line-col-3' },
+
+  // Diagonals
+  { rule: [0, 4, 8], winningLineClass: 'winning-line-diagonal-1' },
+  { rule: [2, 4, 6], winningLineClass: 'winning-line-diagonal-2' },
+];
